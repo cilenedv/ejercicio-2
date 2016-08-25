316e8616727034920d47ccbe84a5fc176db78297
cilenedv <cilenedelacruz@gmail.com>
2016-08-25 15:16:12 -0500

    Subiendo cambios en var

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var numero1=parseInt(prompt("Escribe el primer numero"));
 var numero2=parseInt(prompt("Escribe el segundo numero"));
 var numero3=parseInt(prompt("Escribe el tercer numero"));
-promedio=(numero1+numero2+numero3)/3;
+var promedio=(numero1+numero2+numero3)/3;
 document.write(promedio);
 
